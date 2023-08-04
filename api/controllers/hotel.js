dd5e82fe6fc6662802eac2c974f778912fb6e50b
sheshshakya <sheshshakya101@gmail.com>
2023-08-04 12:31:08 +0530

    added client

---
+++ api/controllers/hotel.js
@@ -0,0 +1,64 @@
+import Hotel from "../models/Hotel.js"
+
+export const createHotel = async (req,res,next)=>{
+    const newHotel = new Hotel(req.body)
+    try{
+        const saveHotel = await newHotel.save()
+        res.status(200).json(saveHotel);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const updatedHotel = async (req,res,next)=>{
+    const newHotel = new Hotel(req.body)
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, 
+            {$set: req.body},{new:true});
+        res.status(200).json(updatedHotel);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const deleteHotel = async (req,res,next)=>{
+    const newHotel = new Hotel(req.body)
+    try{
+        await Hotel.findByIdAndUpdate(req.params.id)
+        res.status(200).json("Hotel has been deleted");
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getHotel = async (req,res,next)=>{
+    const newHotel = new Hotel(req.body)
+    try{
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getHotels = async (req,res,next)=>{
+    const newHotel = new Hotel(req.body)
+    try{
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const countByCity = async (req,res,next)=>{
+    const cities = req.query.cities.split(",")
+    try{
+        const list = await Promise.all(cities.map(city=>{
+            return Hotel.countDocuments({city:city})
+        }))
+        res.status(200).json(list);
+    }catch(err){
+        next(err);
+    }
+}
---
+++ api/routes/hotels.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updatedHotel, countByCity} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js";
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updatedHotel);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/find:id", getHotel);
+
+//GET ALL 
+router.get("/", getHotels);
+router.get("/countByCity",countByCity);
+router.get("/countByType",getHotels);
+
+export default router;
